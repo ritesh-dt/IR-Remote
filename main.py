5936806bc4c242eee754f039b6a35ddd51306c44
Ritesh Thakur <Ritesh@Devendras-MacBook-Pro.local>
2020-03-31 21:29:31 +0530

    Added remote deletion completely and only caption updation screen

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from io import BytesIO
 from zipfile import ZipFile
 
 
-
-
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
 from kivy.uix.boxlayout import BoxLayout
@@ -51,14 +49,37 @@ currentRemote = -1
 
 root_screen_manager = None
 class RootScreenManager(ScreenManager):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
+	title = StringProperty()
+	currentRemote = NumericProperty()
+	currentCaption = StringProperty()
 
 class IRRemote(MDApp):
 	def build(self):
 		global root_screen_manager
 		root_screen_manager = RootScreenManager()
 		return root_screen_manager
+
+	def delete_remote(self):
+		global remoteList, currentRemote
+		with open("remotes.txt", "w") as remoteTxt:
+			index = remoteList.index(str(currentRemote))
+			del remoteList[index:index+3]
+			for file in os.listdir(f"Remotes/{currentRemote}"):
+				os.remove(os.path.join(f"Remotes/{currentRemote}", file))
+			os.rmdir(f"Remotes/{currentRemote}")
+			#print(remoteList)
+			for line in remoteList:
+				remoteTxt.write(line + os.linesep)
+
+			self.updateDrawer()
+			if remoteList != []:
+				currentRemote = int(remoteList[0])
+				self.currentRemote = currentRemote
+				self.currentCaption = remoteList[2]
+				self.transition("Remote", "left", remoteList[1], currentRemote)
+			else:
+				self.transition("No added remotes...", "left")
+
 
 	def findItems(self, dropdown_type, selection, caption=None):
 		global remoteList
@@ -95,17 +116,22 @@ class IRRemote(MDApp):
 			#self.root.ids.model_dropdown.current_item = response.text.split("|")[0]
 
 	def on_start(self):
-		global remoteList
+		global remoteList, currentRemote
 		request_data = {
 							"dropdown_type": "None",
 							"selection": "None"
 						}
 		response = requests.get(DATABASE_URL, data=request_data)
-		#file = BytesIO(response.content)
-		#print(response.text)
 		self.root.ids.appliance_dropdown.items = response.text.split("|")
 		remoteList = [line.replace(os.linesep, "") for line in open("remotes.txt", "r").readlines()]
 		self.updateDrawer()
+		if remoteList != []:
+			currentRemote = int(remoteList[-3])
+			self.currentRemote = currentRemote
+			self.currentCaption = remoteList[-1]
+			self.transition("Remote", "left", remoteList[-2], currentRemote)
+		else:
+			self.transition("No added remotes...", "left")
 
 	def play(self, filename):
 		#print(filename)
@@ -113,6 +139,9 @@ class IRRemote(MDApp):
 			soundDict[filename].play()
 		else:
 			print(f"{filename} not in soundDict")
+
+	def save(self, new_caption):
+		pass
 
 
 	def transition(self, name, direction, remoteType=None, remoteId=None):
@@ -123,24 +152,32 @@ class IRRemote(MDApp):
 			buttonLayout.pos_hint = {"x":0, "top":0.9}
 			buttonLayout.size_hint = (1, 0.9)
 			self.root.ids.remote_screen.add_widget(buttonLayout)
-			secondary_text = remoteList[remoteList.index(str(remoteId).split(".")[0])+2]
-			self.root.ids.md_toolbar.title = secondary_text if secondary_text else remoteList[remoteList.index(str(remoteId).split(".")[0])+1]
+			secondary_text = remoteList[remoteList.index(str(int(remoteId)))+2]
 			
+			self.root.title = secondary_text
+			self.root.ids.md_toolbar.title = secondary_text if secondary_text else remoteList[remoteList.index(str(remoteId).split(".")[0])+1]
 			currentRemote = int(remoteId)
+			self.currentRemote = currentRemote
+			self.currentCaption = secondary_text
 			soundDict = {}
 			for audioFile in os.listdir(f"Remotes/{int(currentRemote)}"):
 				if audioFile.split(".")[-1].lower() in ["wav", "obb"]:
 					soundDict[audioFile.split(".")[0]] = SoundLoader.load(f"Remotes/{currentRemote}/{audioFile}")
 			#print(soundDict)
+		else:
+			self.root.title = name
 		self.root.ids.screen_manager.direction = direction
 		self.root.ids.screen_manager.current = name
 
 	def updateDrawer(self):
 		if remoteList != ['']:
 			content_nav_drawer = self.root.ids.content_nav_drawer.children[0].children[0]
-			for child in content_nav_drawer.children:
-				if isinstance(child, TwoLineItemDrawer):
-					content_nav_drawer.remove_widget(child)
+			childList = content_nav_drawer.children.copy()
+			for index in range(len(childList)):
+				if isinstance(childList[index], TwoLineItemDrawer):
+					content_nav_drawer.remove_widget(childList[index])
+				#else:
+					#print(childList[index].text)
 			for index in range(0, len(remoteList), 3):
 				item = TwoLineItemDrawer(text=remoteList[index+2], secondary_text=remoteList[index+1], icon=iconDict[remoteList[index+1]])
 				#item.on_press = self.transition("Remote", "left")
